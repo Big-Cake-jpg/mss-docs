922edde22b1c89c341db6ab394976b5f56dd265b
Big-Cake-jpg <2577117205@qq.com>
2022-08-18 16:13:50 +0800

    fix: CI error

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,4 @@
-const { defaultTheme } = require('vuepress')
+const { defaultTheme } = require('@vuepress/theme-default')
 const { searchPlugin } = require('@vuepress/plugin-search')
 module.exports = {
   plugins: [
