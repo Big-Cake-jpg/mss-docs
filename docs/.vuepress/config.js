470e9832edf3e1cf634f799350e8efed9559c649
Big-Cake-jpg <2577117205@qq.com>
2022-08-21 12:28:39 +0800

    fix: meta

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -68,6 +68,6 @@ module.exports = {
   head: [
     ['link', { rel: 'icon', href: '/favicon.png' }],
     ['link', { rel: 'apple-touch-icon', href: '/favicon.png' }],
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'theme-color', content: '#45b1e1' }],
   ]
 }
